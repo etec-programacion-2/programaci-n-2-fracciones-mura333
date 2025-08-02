14278973ea889b522b0d724bf565e3c83b18e407
mura333 <ms.mura@alumno.etec.um.edu.ar>
2025-08-01 23:37:05 -0300

    Update Fraccion.kt

--- app/src/main/kotlin/org/example/Fraccion.kt
+++ app/src/main/kotlin/org/example/Fraccion.kt
@@ -8,17 +8,61 @@ class Fraccion(private var _numerador: Int, private var _denominador: Int) {
 
     var denominador: Int = _denominador
         get() = field
-        set(value) { field = value
+        set(value) {
+            if (value == 0) {
+                throw IllegalArgumentException("El denominador no puede ser cero")
+            }
+            field = value
         }
 
-    init { if (denominador == 0){
-        throw IllegalArgumentException("El denominador no puede ser cero")}}
+    init {
+        if (denominador == 0) {
+            throw IllegalArgumentException("El denominador no puede ser cero")
+        }
+    }
 
     override fun toString(): String {
-        return ("$_numerador/$_denominador")
+        return "$numerador/$denominador"
     }
 
-    fun mostrar(){
+    fun mostrar() {
         println(this.toString())
     }
+
+    private fun _simplificar(numerador: Int, denominador: Int): Fraccion {
+        var a = numerador
+        var b = denominador
+        while (b != 0) {
+            val temp = b
+            b = a % b
+            a = temp
+        }
+        val mcd = a
+        return Fraccion(numerador / mcd, denominador / mcd)
+    }
+
+    operator fun plus(otra: Fraccion): Fraccion {
+        val sumaNumerador = this.numerador * otra.denominador + otra.numerador * this.denominador
+        val sumaDenominador = this.denominador * otra.denominador
+        return _simplificar(sumaNumerador, sumaDenominador)
+    }
+
+    operator fun minus(otra: Fraccion): Fraccion {
+        val restaNumerador = this.numerador * otra.denominador - otra.numerador * this.denominador
+        val restaDenominador = this.denominador * otra.denominador
+        return _simplificar(restaNumerador, restaDenominador)
+    }
+}
+
+fun main() {
+    val f1 = Fraccion(1, 2)
+    val f2 = Fraccion(1, 3)
+
+    val suma = f1 + f2
+    val resta = f1 - f2
+
+    println("Fracción 1: $f1")
+    println("Fracción 2: $f2")
+    println("Suma: $suma")
+    println("Resta: $resta")
 }
